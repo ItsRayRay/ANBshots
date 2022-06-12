5c9bea1750c4392fc510cfc20b0407a7ab1fd07b
ItsRayRay <r.boerema21@gmail.com>
2022-06-12 13:20:16 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,18 @@
+const modal = document.getElementById("modal")
+const modalBtn = document.getElementById("modalBtn")
+
+
+const testButton = document.querySelector(".links_name")
+
+
+testButton.addEventListener("click", openModal);
+
+
+
+
+function openModal() {
+    modal.style.display = "block";
+}
+
+
+
